2a8c743255ccf8e01fbb00305711eb9b3232b3e8
Philippe ANSELME-VATIN <pavtocode@gmail.com>
2024-05-20 21:09:21 -0400

    feat: add button and label to display selected dinner

--- WhatsForDinnerTonight/MainPage.xaml.cs
+++ WhatsForDinnerTonight/MainPage.xaml.cs
@@ -3,6 +3,12 @@
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	private string[] Recipes = new string[] {
+		"Redang", 
+		"Lentilles Saucisses", 
+		"Quiche Lorraine"
+		} ;
+	Random random = new Random();
 
 	public MainPage()
 	{
@@ -19,6 +25,16 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private void OnReceipRequest(object sender, EventArgs e)
+	{ 
+		int randomIndex = random.Next(0,Recipes.Length);
+		System.Console.WriteLine("index: " + randomIndex);
+		SelectedRecipe.Text	 = $" {Recipes[randomIndex]}";
+		SemanticScreenReader.Announce(SelectedRecipe.Text);
+
+
 	}
 }
 
